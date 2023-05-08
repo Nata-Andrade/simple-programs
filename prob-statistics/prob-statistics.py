dd7aa3144d6dd776d1315335de5a93ba862a6489
Natã Andrade <73320913+Nata-Andrade@users.noreply.github.com>
2023-05-08 12:54:27 -0300

    Update prob-statistics.py

--- prob-statistics/prob-statistics.py
+++ prob-statistics/prob-statistics.py
@@ -1,8 +1,8 @@
 #Program for calculating things related to Probability and Statistics
 
 print("""Insert a value related to the desired operation:
-\tArithmetic Mean (0)
-\tMode (1)
-\tMedian (2)
-\tStd Deviation (3)
-\tLinear Correlation Coefficient (4)""")
+\tArithmetic Mean (0);
+\tMode (1);
+\tMedian (2);
+\tStd Deviation (3);
+\tLinear Correlation Coefficient (4);""")
