784fabe9b5916ff183152a2be4680fbe44736d29
Natã Andrade <73320913+Nata-Andrade@users.noreply.github.com>
2023-08-08 18:41:35 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void low_to_up(char* str){
+	for(int i = 0; i < strlen(str); i++) str[i] = str[i] -(32*(str[i]>90)) +(32*(str[i]<91));
+}
+
+int main(void){
+	
+	char str[] = "stRiNg\0";
+	printf("%s\n", str);
+	low_to_up(str);
+	printf("%s\n", str);
+	
+	return 0;
+}
